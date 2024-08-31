c82ec324f65fe21bfc131d1f7ca792f41b451fbf
hbghaffar01 <hbghaffar01@gmail.com>
2024-08-31 19:52:56 +0400

    Updated Code

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import { Route, Routes, Navigate } from "react-router-dom";
 import AllQuotes from "./components/pages/AllQuotes";
 import QuoteDetail from "./components/pages/QuoteDetail";
 import NewQuotes from "./components/pages/NewQuote";
+import Layout from "./components/layout/Layout";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" exact element={<Navigate to="/quotes" />} />
-      <Route path="/quotes" exact element={<AllQuotes />} />
-      <Route path="/quotes/:quoteId/*" element={<QuoteDetail />} />
-      <Route path="/new-quote" element={<NewQuotes />} />
-    </Routes>
+    <Layout>
+      <Routes>
+        <Route path="/" exact element={<Navigate to="/quotes" />} />
+        <Route path="/quotes" exact element={<AllQuotes />} />
+        <Route path="/quotes/:quoteId/*" element={<QuoteDetail />} />
+        <Route path="/new-quote" element={<NewQuotes />} />
+      </Routes>
+    </Layout>
   );
 }
 
